a26eca8c77b93324bfcb744725425a4db70232ae
pipwells <33559860+pipwells@users.noreply.github.com>
2025-09-08 16:15:24 +1000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isCI = process.env.VERCEL === "1" || process.env.CI === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Don’t fail the build on lint errors when running in Vercel/CI
+    ignoreDuringBuilds: isCI,
+  },
 };
 
 export default nextConfig;
