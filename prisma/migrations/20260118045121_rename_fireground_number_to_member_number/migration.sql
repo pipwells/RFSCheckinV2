0394fc331cd23d505f5acbc3c8c9419b99c8d425
Pip Wells <pipwells101@gmail.com>
2026-01-18 16:08:55 +1100

    Rename firegroundNumber to memberNumber

---
+++ prisma/migrations/20260118045121_rename_fireground_number_to_member_number/migration.sql
@@ -0,0 +1,2 @@
+-- Rename column, preserving existing data and constraints
+ALTER TABLE "Member" RENAME COLUMN "firegroundNumber" TO "memberNumber";
---
+++ prisma/migrations/20260118045247_firegroundto_member/migration.sql
@@ -0,0 +1,2 @@
+-- RenameIndex
+ALTER INDEX "public"."Member_organisationId_firegroundNumber_key" RENAME TO "Member_organisationId_memberNumber_key";
